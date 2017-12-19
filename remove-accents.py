3a30137c2b8286ed4310c327e31b3b706899b257
Annette von Stockhausen <annette@vonstockhausen.eu>
2017-12-19 21:44:49 +0100

    Add trema

--- remove-accents.py
+++ remove-accents.py
@@ -11,5 +11,5 @@ with open(infile, 'r') as fin:
     contents = fin.read()
     
 with open(outfile, 'w') as fout:
-    newcontents = unicodedata.normalize("NFKD", contents).translate({ord(c): None for c in "̓̔́̀͂ͅ"})
+    newcontents = unicodedata.normalize("NFKD", contents).translate({ord(c): None for c in "̓̔́̀͂̈ͅ"})
     fout.write(newcontents)
